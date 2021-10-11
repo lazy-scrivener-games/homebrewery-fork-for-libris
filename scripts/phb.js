afe40e7cf87ad22708775a8b22dadf3cfd7b90c5
caesuric <illusorycaesura@gmail.com>
2021-10-11 11:13:28 -0400

    updated to use system fonts for Weasyprint, various tweaks for PDF conversion

--- scripts/phb.js
+++ scripts/phb.js
@@ -1,7 +1,7 @@
 const less = require('less');
 const fs = require('fs');
 
-less.render(fs.readFileSync('./client/homebrew/phbStyle/phb.style.less', 'utf8'), { compress: true })
+less.render(fs.readFileSync('./themes/5ePhbLegacy.style.less', 'utf8'), { compress: true })
 	.then((output)=>{
 		fs.writeFileSync('./phb.standalone.css', output.css);
 		console.log('phb.standalone.css created!');
