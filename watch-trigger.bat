a2545d491ee437262d8133beffb3d87a80709ebe
caesuric <illusorycaesura@gmail.com>
2021-10-14 07:59:06 -0400

    updated watch batch script

--- watch-trigger.bat
+++ watch-trigger.bat
@@ -1,6 +1,6 @@
 call npm run phb
 copy phb.standalone.css ..\enki-dnd-style\5ePhb.style.css
 cd ..\enki-dnd-style
-call wsl python3 ../enki/enki.py example-config.json
+call wsl python3 -m enki example-config.json
 cd ..\homebrewery
 echo PDF generated!
